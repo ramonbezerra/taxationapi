8802cda2e5605ea1d13e536d628c1e1c0c865128
ramonbezerra <ramon.bezerra@outlook.com>
2022-07-21 01:13:51 -0300

    first commit

---
+++ src/main/java/com/enterprise/taxationapi/TaxationapiApplication.java
@@ -0,0 +1,13 @@
+package com.enterprise.taxationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxationapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxationapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enterprise/taxationapi/TaxationapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enterprise.taxationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxationapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
