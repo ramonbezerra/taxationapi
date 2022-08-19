d9250e16e9bd7911497bdc5eb23207dab2d76b87
ramonbezerra <ramon.bezerra@outlook.com>
2022-08-19 17:27:02 -0300

    add jhipster-test to gitignore

--- jhipster-test/store/src/main/docker/mongodb/MongoDB.Dockerfile
+++
@@ -1,2 +0,0 @@
-FROM mongo:4.4.14
-ADD mongodb/scripts/init_replicaset.js init_replicaset.js
--- jhipster-test/store/src/main/java/com/okta/developer/store/config/dbmigrations/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * MongoDB database migrations using Mongock.
- */
-package com.okta.developer.store.config.dbmigrations;
