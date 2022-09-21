ba0d6167ef5719a8cd5d59f562b091cc21be12e1
ramonbezerra <ramon.bezerra@outlook.com>
2022-09-20 22:55:15 -0300

    exception rename

--- src/main/java/com/enterprise/taxationapi/service/CompanyService.java
+++ src/main/java/com/enterprise/taxationapi/service/CompanyService.java
@@ -6,13 +6,13 @@ import org.springframework.stereotype.Service;
 
 import com.enterprise.taxationapi.domain.Company;
 import com.enterprise.taxationapi.exceptions.CompanyNotFoundException;
-import com.enterprise.taxationapi.exceptions.ExistingCompanyExcecption;
+import com.enterprise.taxationapi.exceptions.ExistingCompanyException;
 
 
 @Service
 public interface CompanyService {
 
-    public Company createCompany (Company company) throws ExistingCompanyExcecption;
+    public Company createCompany (Company company) throws ExistingCompanyException;
 
     public Company updateCompany (Long id, Company company);
 
